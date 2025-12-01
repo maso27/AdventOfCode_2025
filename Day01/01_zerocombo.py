df74ce8780a24b96c3129b1f16bbb2f8c5959d50
Stone, Mason <mason.stone@ballaerospace.com>
2025-12-01 09:27:52 -0700

    day 1 first half untested

---
+++ Day01/01_zerocombo.py
@@ -0,0 +1,27 @@
+filename = './Day01/sample.txt'
+verbose = 3
+
+f = open(filename, 'r')
+lines = f.readlines() # reading all lines
+# line = f.readline()  # reading one line
+f.close()
+
+needlepoint = 50
+num_zeros = 0
+
+for line in lines:
+    turn_dir = list(line)[0] # grab the first character
+    clicks = int(line[1:])
+    if verbose >= 2:
+        print(f"  Turning {turn_dir} and moving {clicks} clicks from {needlepoint}", end='')
+    if turn_dir.upper() == "L":
+        clicks = -clicks
+    needlepoint = (needlepoint + clicks) % 100
+    if verbose >= 2:
+        print(  f" to {needlepoint}")
+    if needlepoint == 0:
+        num_zeros += 1
+        if verbose >= 1:
+            print(f"Hit zero at line: {line}")
+
+print(f"\nDone.\n  Total zero hits: {num_zeros}\n")
