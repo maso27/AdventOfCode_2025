8bb6036e642e57c7c1f60530e14738d792fb871a
Stone, Mason <mason.stone@ballaerospace.com>
2025-12-10 14:11:34 -0700

    still on Day 8.1

--- Day08/01_flanning.py
+++ Day08/01_flanning.py
@@ -1,11 +1,11 @@
-filename = './Day08/sample.txt'
-verbose = 0
+filename = './Day08/input.txt'
+verbose = 1
 
 import pyflann as pf # had to pip import pyflann-py3d
 import numpy as np
 
-NUM_CONNECTIONS = 10
-SEARCH_DEPTH = 5
+NUM_CONNECTIONS = 1000
+SEARCH_DEPTH = 100;
 
 f = open(filename, 'r')
 lines = f.readlines() # reading all lines
@@ -16,12 +16,11 @@ locations = []
 for line in lines:
     line = line.rstrip()
     parts = line.split(',')
-    x = float(parts[0])
-    y = float(parts[1])
-    z = float(parts[2])
+    x,y,z = float(parts[0]), float(parts[1]), float(parts[2])
     locations.append((x,y,z))
 
 locations = np.array(locations)
+
 
 if verbose >= 4:
     print (locations)
@@ -42,15 +41,17 @@ shortest_indexes = np.delete(shortest_indexes, np.arange(1, len(shortest_indexes
 
 if verbose >= 3:
     print(f'shortest distances: {shortest_indexes}')
+if verbose >= 1:
+    print(f'length of the list of shortest distances: {len(shortest_indexes)}')
 
 # figure out points based on index
 lines_list = []
 for idx in shortest_indexes[:NUM_CONNECTIONS]:
     point1 = int(idx / (SEARCH_DEPTH - 1)) # first point is the row
     point2 = result.flatten()[idx]         # second point is where the index is pointing
-    if verbose >= 2:
+    if verbose >= 3:
         print(f'Point {point1} is distance: {dists.flatten()[idx]} from point {point2}')
-    if verbose >= 1:
+    if verbose >= 2:
         print(f'  {locations[point1]} to {locations[point2]}')
     lines_list.append([point1,point2])
 
@@ -64,9 +65,9 @@ for a in range(len(locations)):
     a_spots = np.array(a_spots).T # output is row then column of locations
     if a_spots.size == 0: # empty, move to next one
         continue
-    if verbose >= 2:
+    if verbose >= 3:
         print(f'location {a}')
-        print(a_spots)
+        # print(a_spots)
     this_circuit.append(a)
     for spot in a_spots:
         other_spot = (spot[1] + 1) % 2 # toggle 1 or 0
@@ -90,9 +91,11 @@ if verbose >= 2:
 circuit_lengths = []
 for circuit in circuits:
     circuit_lengths.append(len(circuit))
-circuit_lengths.sort()
+circuit_lengths.sort(reverse=True)
+if verbose >= 2:
+    print(f'\nlengths of top 10 circuits, sorted: {circuit_lengths[:10]}')
 
 print('\nDone.')
-print(f'lengths of 3 longest circuits multiplied: {circuit_lengths[-1] * circuit_lengths[-2] * circuit_lengths[-3]}')
+print(f'lengths of 3 longest circuits multiplied: {circuit_lengths[0] * circuit_lengths[1] * circuit_lengths[2]}')
 
 # 30492 is too low
